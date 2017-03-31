bde5ff354f9c91e88088a524cc9c72db309711f2
Fuyang Liu <liufu.ty@gmail.com>
2017-03-31 22:38:20 +0200

    Add some kera code

--- tf-learn/example-3/create_sentiment_featuresets.py
+++ tf-learn/example-3/create_sentiment_featuresets.py
@@ -27,7 +27,7 @@ def create_lexicon(pos, neg):
                 lexicon += list(all_words)
     lexicon = [lemmatizer.lemmatize(i) for i in lexicon]
     w_counts = Counter(lexicon)
-    
+
     l2 = []
     for w in w_counts:
         if 1000 > w_counts[w] > 50:
@@ -36,14 +36,14 @@ def create_lexicon(pos, neg):
 
 def sample_handling(sample, lexicon, classification):
     featureset = []
-    
+
     with io.open(sample, 'r', encoding='utf-8') as f:
         contents = f.readlines()
         for l in contents[:hm_lines]:
             current_words = word_tokenize(l.lower())
             current_words = [lemmatizer.lemmatize(i) for i in current_words]
             features = np.zeros(len(lexicon))
-            
+
             for word in current_words:
                 if word.lower() in lexicon:
                     index_value = lexicon.index(word.lower())
@@ -58,30 +58,34 @@ def create_feature_sets_and_labels(pos, neg, test_size=0.2):
     features += sample_handling('pos.txt', lexicon, [1,0])
     features += sample_handling('neg.txt', lexicon, [0,1])
     random.shuffle(features)
-    
+
     features = np.array(features)
-    
+
     testing_size = int(test_size * len(features))
-    
+
     train_x = list(features[:, 0][:-testing_size]) # getting the features
     train_y = list(features[:, 1][:-testing_size])
-    
+
     test_x = list(features[:, 0][-testing_size:]) # getting the features
     test_y = list(features[:, 1][-testing_size:])
-    
-    return train_x, train_y, test_x, test_y
-    
+
+    return train_x, train_y, test_x, test_y, lexicon
+
 if __name__ == '__main__':
-    train_x, train_y, test_x, test_y = \
+    train_x, train_y, test_x, test_y, lexicon = \
         create_feature_sets_and_labels('pos.txt', 'neg.txt', test_size=0.2)
-    
+
     # dump pickle file ...
     filename = 'tmp/sentiment_set.pickle'
+    filename_lexicon = 'tmp/lexicon.pickle'
     if not os.path.exists(os.path.dirname(filename)):
         try:
             os.makedirs(os.path.dirname(filename))
         except OSError as exc: # Guard against race condition
             if exc.errno != errno.EEXIST:
                 raise
+
     with open(filename, 'wb') as f:
         pickle.dump([train_x, train_y, test_x, test_y], f)
+    with open(filename_lexicon, 'wb') as f:
+        pickle.dump(lexicon, f)
