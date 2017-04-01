c3d001aa791de8484be7b263a5b144f7d9b80332
Fuyang Liu <fli@tradeshift.com>
2017-04-01 18:17:48 +0200

    Update tf-learn example3

--- tf-learn/example-3-sentiment/example3.kera.py
+++ tf-learn/example-3-sentiment/example3.kera.py
@@ -1,7 +1,4 @@
-'''Trains a simple deep NN on the MNIST dataset.
-Gets to 98.40% test accuracy after 20 epochs
-(there is *a lot* of margin for parameter tuning).
-2 seconds per epoch on a K520 GPU.
+'''Trains a simple sentiment analysis model.
 '''
 
 from __future__ import print_function
